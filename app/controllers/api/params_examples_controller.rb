f042a0b54fc49e912c2ebe9ef63b991d9c33267d
JackMarx <seriousfools@gmail.com>
2019-01-17 10:01:15 -0600

    initial commit

---
+++ app/controllers/api/params_examples_controller.rb
@@ -0,0 +1,10 @@
+class Api::ParamsExamplesController < ApplicationController
+  def string_example
+    @message = params[:bologna]
+    @message_2 = params[:tent]
+
+    @bologna_message_length = @message.length
+    
+    render 'string_example.json.jbuilder'
+  end
+end
